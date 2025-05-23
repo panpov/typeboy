c013212a529952e622da2848d66dc9a8fc5c8922
jun0032 <jxseo@hamilton.edu>
2025-05-22 22:09:47 -0400

    buggy?

--- src/type1.asm
+++ src/type1.asm
@@ -1,23 +1,33 @@
 include "src/utils.inc"
 section "type1", rom0
 
+def TILEID_A                         equ ($80)
+def TILEID_Z                         equ ($99)
+
+
 type1:
     ld a, [JOYPAD_PRESSED_ADDRESS]
     and PADF_RIGHT
     jr nz, .right_not_pressed
 
+    WriteTile $9905, [CURR_CHARACTER]
+    AddBetter [CURR_CHARACTER], 1
     ; call next_character
 
     .right_not_pressed
     ret
 
-; next_character:
-;     AddBetter [CURR_CHAR], 1
+next_character:
+    AddBetter [CURR_CHARACTER], 1
 
-;     ; check if we need to wrap around
-;     cp $9A
-;     jr nz, .not_wrap_around
+    ; check if we need to wrap around
+    cp $9A
+    jr nz, .not_wrap_around
 
-;     ; wrap around
-;     ld a, 0x00
-;     ld [CHARACTER], a
+    ; wrap around
+    Copy [CURR_CHARACTER], TILEID_A
+
+    .not_wrap_around
+    ret
+
+export type1, next_character
